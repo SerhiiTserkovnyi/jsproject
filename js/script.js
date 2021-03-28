73d0e26d0ad11a917c2422c840d8b79fca8a28d0
sergiy413 <dizz413@gmail.com>
2021-03-28 21:28:29 +0300

    my first project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let resultat = [];
+resultat[0] = prompt("Are you" , "");
+ resultat[1] = prompt("Are you" , "");
+ resultat[2] = prompt("Are you" , "");
+ document.write (resultat);
+
