3eb199b3a05216d2e0d8970c00272d279645659a
sergiy413 <dizz413@gmail.com>
2021-03-29 21:12:39 +0300

    task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-"use strict";
-let resultat = [];
-resultat[0] = prompt("Are you" , "");
- resultat[1] = prompt("Are you" , "");
- resultat[2] = prompt("Are you" , "");
- document.write (resultat);
+const numberOfFilms = +prompt ('Скільки фільмів Ви вже переглянули?'  , '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх переглянутих фільмів?' , ''),
+    b = prompt('Яку ви поставите оцінку фільмові' , ''),
+    c = prompt('Один з останніх переглянутих фільмів?' , ''),
+    d = prompt('Яку ви поставите оцінку фільмові' , '');
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
 
