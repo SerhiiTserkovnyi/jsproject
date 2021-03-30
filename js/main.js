8c479e1021a9e1cd5b21ce4c6149405e78c2b1cf
sergiy413 <dizz413@gmail.com>
2021-03-30 13:47:56 +0300

    Кінолбю з циклом

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let numm = 50;
+while (numm <= 55){
+    document.write(numm);
+    numm++
+}
+do {
+    document.write(numm);
+    numm++
+}
+while(numm <= 55);
--- js/script.js
+++ js/script.js
@@ -7,11 +7,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один з останніх переглянутих фільмів?' , ''),
-    b = prompt('Яку ви поставите оцінку фільмові' , ''),
-    c = prompt('Один з останніх переглянутих фільмів?' , ''),
-    d = prompt('Яку ви поставите оцінку фільмові' , '');
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+
+    for (let i = 0; i < 2; i++) {
+        a = prompt('Один з останніх переглянутих фільмів?' , ''),
+        b = prompt('Яку ви поставите оцінку фільмові' , '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length <50){
+            personalMovieDB.movies[a] = b;
+            console.log ('done');
+        } else {
+            console.log ('error');
+            i--;
+        }    
+
+    }
+    if (personalMovieDB.count < 10) {
+        console.log ('Переглянуто дуже мало фільмів');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log ('Ви класичний глядач');
+    } else if (personalMovieDB.count >= 30){
+        console.log('Ви кіноман');
+    } else {
+        console.log('Виникла помилка');
+    }
+
     console.log(personalMovieDB);
 
